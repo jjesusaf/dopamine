7ac3403b0e335107c077a09a5be24f122f0b8497
José de Jesús Acevedo Flores <jjaf@MacBook-Air-de-Jose-2.local>
2024-02-25 02:24:50 -0600

    Removed unnecessary components

--- src/components/ProductGallery.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import Image from "next/image";
-
-export const ProductGallery: FC = () => (
-  <div className="flex w-full md:w-3/5 lg:w-2/3 flex-col gap-10">
-    {/* Contenido de ProductGallery omitido por brevedad */}
-  </div>
-);
